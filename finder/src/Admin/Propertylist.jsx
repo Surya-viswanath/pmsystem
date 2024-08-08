8715874222cdcb578a9fb0176589d65977b3ca98
Surya-viswanath <121759302+Surya-viswanath@users.noreply.github.com>
2024-08-08 21:41:41 +0530

    Update Propertylist.jsx

--- finder/src/Admin/Propertylist.jsx
+++ finder/src/Admin/Propertylist.jsx
@@ -1,79 +1,4 @@
-// import axios from 'axios';
-// import React, { useContext, useEffect, useState } from 'react'
-// import { Link, useNavigate } from 'react-router-dom'
-// import { MdDelete } from "react-icons/md";
-// import './Admin.css'
 
-// function Propertylist() {
-    
-//  const [Property, setProperty] = useState([])
- 
-//     useEffect(() => {
-//         const handleItems = async () => {
-//           try {
-//             const response = await axios.get('http://localhost:4008/getpro');
-//             setProperty(response.data);
-//           } catch (error) {
-//             console.error('Error fetching data:', error);
-//           }
-//         };
-    
-//         handleItems();
-//       }, []);
-//     const handleDelete = async (id) => {
-//         try {
-//           await axios.delete(`http://localhost:4008/deleteList/${id}`);
-//           // Update the state after successful deletion
-//           setProperty(Property.filter(list => list._id !== id));
-//         } catch (error) {
-//           console.error('Error deleting list:', error);
-//         }
-//       };
-//   return (
-//     <div>
-   
-//          <div className="main-head section-p1">
-//          <h3 className='head-restaurants'>Agent List</h3>
-//     <table width="100%" className='table-border '>
-   
-// <thead style={{padding:'20px'}}>
-//     <tr className='table'>
-//        <td className='table-head'>Type</td>
-//         <td className='table-head'>Image</td>
-//         <td className='table-head'>Address</td>
-//         <td className='table-head'>Price</td>
-//         <td className='table-head'>Property size</td>
-//         <td className='table-head'>Email</td>
-//         <td className='table-head'>Remove</td>
-        
-//     </tr>
-// </thead>
-// <tbody style={{padding:'20px'}}>
-// {Property.map(display =>(
-//     <tr className='table'>
-//         <td>{display.sell}</td>
-//         <td><img src= {display.image} alt="" width={'100px'} height={'100px'}  style={{marginBottom:'10px',maxHeight:'150px'}}/></td>
-//         <td>{display.address}</td>
-//         <td>{display.regularPrice}</td>
-//         <td>{display.propertysize}</td>
-//         <td>{display.email}</td>
-//         <td><MdDelete className='delete-icon' onClick={() => handleDelete(display._id)}/> </td>
-       
-        
-//     </tr>
-    
-//     ))}
-// </tbody>
-//     </table>
-//     <div className='normal-btn'>
-    
-//     </div>
-//     </div>
-//     </div>
-//   )
-// }
-
-// export default Propertylist
 
 
 
@@ -90,7 +15,7 @@ const Propertylist = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get('http://localhost:4008/getpro');
+        const response = await axios.get('https://pmsystem-4.onrender.com/getpro');
         setUsers(response.data);
       } catch (error) {
         console.error('Error fetching users:', error);
@@ -102,7 +27,7 @@ const Propertylist = () => {
 
   const handleDelete = async (id) => {
     try {
-      await axios.delete(`http://localhost:4008/deleteList/${id}`);
+      await axios.delete(`https://pmsystem-4.onrender.com/deleteList/${id}`);
       // Update the state after successful deletion
       setUsers(users.filter(user => user._id !== id));
       toast.success('User deleted successfully');
