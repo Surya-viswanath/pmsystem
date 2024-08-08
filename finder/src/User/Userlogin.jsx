86cfc3010115b2e0a9433f9015c0d54409414956
Surya-viswanath <121759302+Surya-viswanath@users.noreply.github.com>
2024-08-08 21:54:45 +0530

    Update Userlogin.jsx

--- finder/src/User/Userlogin.jsx
+++ finder/src/User/Userlogin.jsx
@@ -28,7 +28,7 @@ const handlename =(event)=>{
    
       event.preventDefault()
       try{
-      const display =await axios.post('http://localhost:4008/customerlogin',{email, password})
+      const display =await axios.post('https://pmsystem-4.onrender.com/customerlogin',{email, password})
       console.log(display.data);
       // navigate(`/Home/${display.data.id}`);}
       // catch(error){console.log(error); 
