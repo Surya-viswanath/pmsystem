b797247edc61adb982c9af5086bb80e6b449526a
Surya-viswanath <121759302+Surya-viswanath@users.noreply.github.com>
2024-08-08 22:03:26 +0530

    Update Buy.jsx

--- finder/src/components/Buy.jsx
+++ finder/src/components/Buy.jsx
@@ -37,7 +37,7 @@ function Buy() {
   useEffect(() => {
     const handleItems = async () => {
       try {
-        const response = await axios.get('http://localhost:4008/getpro');
+        const response = await axios.get('https://pmsystem-4.onrender.com/getpro');
         setFirst(response.data);
        
        
@@ -65,7 +65,7 @@ const handleAddToWishlist = async (propertyid) => {
       return;
     }
 
-    await axios.post('http://localhost:4008/wishlist', {
+    await axios.post('https://pmsystem-4.onrender.com/wishlist', {
       userId: user._id,
       propertyid,
     });
