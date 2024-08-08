bcc96393f92d64cfb82ea6b3bfb83d3e830a94c3
Surya-viswanath <121759302+Surya-viswanath@users.noreply.github.com>
2024-08-08 22:05:57 +0530

    Update Commercial.jsx

--- finder/src/components/Commercial.jsx
+++ finder/src/components/Commercial.jsx
@@ -22,7 +22,7 @@ function Commercial() {
   useEffect(() => {
     const handleItems = async () => {
       try {
-        const response = await axios.get('http://localhost:4008/getpro');
+        const response = await axios.get('https://pmsystem-4.onrender.com/getpro');
         setFirst(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
