279ef9064fe7cfaabfc26d9ff38d72c80749a9af
Surya-viswanath <121759302+Surya-viswanath@users.noreply.github.com>
2024-08-08 22:01:59 +0530

    Update Banner.jsx

--- finder/src/components/Banner.jsx
+++ finder/src/components/Banner.jsx
@@ -1,67 +1,4 @@
-// import React, { useEffect, useState } from 'react'
-// import './Banner.css'
-// import { Col, Container, Row } from 'react-bootstrap'
-// import banner from './Bannerhome.jpg'
-// import axios from 'axios';
-// import { IoIosSearch } from "react-icons/io";
-// import Searchinput from '../Search/Searchinput';
-// import Propertilist from '../Search/Propertilist';
-// import Category from './Category';
-// function Banner() {
 
-//   const [properties, setProperties] = useState([]);
-//   const [filteredProperties, setFilteredProperties] = useState([]);
-//   const [placeFilter, setPlaceFilter] = useState('');
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-
-//   const fetchData = async () => {
-//     try {
-//       const response = await axios.get('http://localhost:4008/getpro');
-//       setProperties(response.data); // Assuming the API returns an array of properties
-//       setFilteredProperties(response.data); // Initialize filtered properties with all properties
-//     } catch (error) {
-//       console.error('Error fetching data:', error);
-//     }
-//   };
-
-//   const handlePlaceFilterChange = (e) => {
-//     setPlaceFilter(e.target.value);
-//   };
-
-//   const filterPropertiesByPlace = () => {
-//     if (!placeFilter) {
-//       setFilteredProperties(properties); // If no filter, show all properties
-//     } else {
-//       const filtered = properties.filter(property =>
-//         property.sell.toLowerCase().includes(placeFilter.toLowerCase())
-//       );
-//       setFilteredProperties(filtered);
-//     }
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     filterPropertiesByPlace();
-//   };
-
-//   return (
-//    <div>
-//     <div className='banner'>
-//       <Row >
-//         <h1 className='find'>Find your Dream Home here</h1>
-//         <Searchinput value={placeFilter} onChange={handlePlaceFilterChange} onSubmit={handleSubmit} />
-//         {/* <Category/> */}
-//       </Row>
-//     </div>
-//      <Propertilist properties={filteredProperties} />
-//      </div>
-//   )
-// }
-
-// export default Banner
 
 
 
@@ -81,7 +18,7 @@ function Banner() {
 
   useEffect(() => {
     // Fetch properties using Axios
-    axios.get('http://localhost:4008/getpro')
+    axios.get('https://pmsystem-4.onrender.com/getpro')
         .then(response => {
             setProperties(response.data); // Assuming the response contains an array of properties
             setFilteredProperties(response.data); // Initially set filtered properties to all properties
